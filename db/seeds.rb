df885e5c690d570357a0d109fa77c8e5d24d7c9a
Cello Ward <celloward@gmail.com>
2019-12-10 14:50:03 -0600

    Cleaned seed generation code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,49 @@
-def create_schedule watering_dates, start_date, end_date
-    schedule = []
-    (start_date..end_date).map do |current_date|
-      plants_array = []
-      watering_dates.each do |plant_hash|
-        plants_array << plant_hash["name"] if plant_hash["water_on"].include?(current_date)
-      end
-      unless plants_array.empty?
-        schedule << { date: "#{current_date}", plants: plants_array }
-      end
-    end
-    schedule
+def find_dates plant_hash, hash_key, start_date, end_date
+  date = start_date
+  date_array = []
+  until date > end_date
+    date_array << date
+    date += plant_hash[hash_key].split[0].to_i
+    date = avoid_weekends_and_holidays(date)
   end
-  
-  def find_dates plant_info, days_after, start_date, end_date
-      date = start_date
-      date_array = []
-      until date > end_date
-        date_array << date
-        date += plant_info[days_after].split[0].to_i
-        date = avoid_weekends(date)
-      end
-      date_array
-  end
-  
-  def avoid_weekends date
-    case
-    when date.saturday?
-      date = date - 1
-    when date.sunday?
-      date = date + 1
-    when date == Date.strptime("12-25-2019", "%m-%d-%Y")
-      date = date + 1
-    when date == Date.strptime("1-1-2020", "%m-%d-%Y")
-      date = date + 1
-    end
-    date
-  end
+  date_array
+end
 
+def avoid_weekends_and_holidays date
+  if date.saturday?
+    date = date - 1
+  elsif date.sunday? || 
+    date == Date.strptime("12-25-2019", "%m-%d-%Y") || 
+      date == Date.strptime("1-1-2020", "%m-%d-%Y")
+        date = date + 1
+    end
+  date
+end
+
+#Reset Database
 Schedule.delete_all
 
+#Parse JSON data
 watering_json = File.open("db/Apprentice_WeGrowInTandem_Data.json", "r").read
 watering_record = JSON::load(watering_json)
-begin_date = Date.strptime("12-16-2019", "%m-%d-%Y")
-finish_date = begin_date + 84
 
-watering_dates = watering_record.map { |plant_record| plant_record.merge({ "water_on" => (find_dates(plant_record, "water_after", begin_date, finish_date)) }) }
-schedule = Schedule.create!([create_schedule(watering_dates, begin_date, finish_date)])
+#Set time frame for schedule
+start_date = Date.strptime("12-16-2019", "%m-%d-%Y")
+end_date = start_date + 84
 
+#Seed dates in Schedule Table
+schedule = (start_date..end_date).each { |current_date| Schedule.create!({ date: current_date, plants: [] }) }
 
+#Create array with all relevant info
+watering_dates_array = watering_record.map { 
+  |plant_hash| plant_hash.merge({ "water_on" => (find_dates(plant_hash, "water_after", start_date, end_date)) }) }
+
+#Seed plants in Schedule Table
+Schedule.find_each do |current_date|
+  watering_dates_array.each do |plant_hash|
+    if plant_hash["water_on"].include?(Date.strptime("#{current_date.date}", "%Y-%m-%d"))
+      current_date.plants << plant_hash["name"]
+    end
+  end
+  current_date.save
+end
