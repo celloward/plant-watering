1d10c117d2871c85c4766479b471a21669a94d81
Cello Ward <celloward@gmail.com>
2019-12-12 15:41:33 -0600

    Start formatting calendar

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,4 +1,15 @@
 module SchedulesHelper
-  
+
+  def to_date date_string
+    Date.strptime( date_string, "%Y-%m-%d")
+  end
+
+  def collect_months schedule
+    schedule.all.find_each.collect { |r| to_date(r.date).month }.uniq
+  end
+
+  def records_in_month select_month
+    Schedule.all.find_each.select { |r| to_date(r.date, "%Y-%m-%d").month == select_month  }
+  end
 
 end
