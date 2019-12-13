84e5b6e217ea03505fc6d9f96b0924985fc0d09f
Cello Ward <celloward@gmail.com>
2019-12-13 13:18:12 -0600

    Format show page and add color

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -16,4 +16,9 @@ module SchedulesHelper
     date_num = to_date(select_date) .wday - 1
     date_num < 0 ? date_num = 6 : date_num
   end
+
+  def print_date date_string
+    real_date = to_date(date_string)
+    "#{Date::MONTHNAMES[real_date.month]} #{real_date.day}, #{real_date.year}"
+  end
 end
