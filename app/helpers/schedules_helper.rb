74250fb648a57447aa0db3ebb186b5a8f83e6125
Cello Ward <celloward@gmail.com>
2019-12-13 13:55:08 -0600

    Clean up method placements and views

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -4,14 +4,6 @@ module SchedulesHelper
     Date.strptime( date_string, "%Y-%m-%d")
   end
 
-  def collect_months schedule
-    schedule.all.find_each.collect { |r| to_date(r.date).month }.uniq
-  end
-
-  def records_in_month select_month
-    Schedule.all.find_each.select { |r| to_date(r.date).month == select_month  }
-  end
-
   def days_from_monday select_date
     date_num = to_date(select_date) .wday - 1
     date_num < 0 ? date_num = 6 : date_num
